755efccb0a024b5c8f45451902f27fd8cc6af9ed
Will Smith <will@fundapps.co>
2024-10-24 11:26:33 +0100

    Refactor, use a backward for-loop instead of nested whiles

--- Kata/Class1.cs
+++ Kata/Class1.cs
@@ -4,23 +4,16 @@ public class Class1
 {
     public static int[] Do(int amount, int[] values)
     {
-        var index = values.Length - 1;
-
-        var result = values.Select(_ => 0).ToArray();
-        while (amount > 0)
+        var result = new int[values.Length];
+        for (var index = values.Length - 1; index >= 0; index--)
         {
-            while (values[index] > amount)
-            {
-                index--;
-            }
-
             while (amount >= values[index])
             {
                 amount -= values[index];
                 result[index]++;
             }
         }
-
+        
         return result;
     }
 }
