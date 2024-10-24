f677d31343c5c0a73a0cb70a9be032f49de93928
Will Smith <will@fundapps.co>
2024-10-24 11:30:38 +0100

    Refactor + stub test (slow at maths)

--- Kata.Tests/UnitTest1.cs
+++ Kata.Tests/UnitTest1.cs
@@ -5,15 +5,23 @@ public class Tests
     [Test]
     public void Test1()
     {
-        var amount = Class1.Do(15, [1, 2, 5, 10, 20, 50]);
+        var amount = ChangeCalculator.GiveMeMoney(15, [1, 2, 5, 10, 20, 50]);
         
         Assert.That(amount, Is.EqualTo(new [] { 0, 0, 1, 1, 0, 0 }));
     }
     [Test]
     public void Test2()
     {
-        var amount = Class1.Do(45, [1, 2, 5, 10, 20, 50]);
+        var amount = ChangeCalculator.GiveMeMoney(45, [1, 2, 5, 10, 20, 50]);
         
         Assert.That(amount, Is.EqualTo(new [] { 0, 0, 1, 0, 2, 0 }));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        var amount = ChangeCalculator.GiveMeMoney(314159265, [1, 2, 5, 10, 20, 50]);
+        
+        var expected = new[] { 0, 0, 1, 0, 1, 0, 0 };
     }
 }
---
+++ Kata/ChangeCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Kata;
+
+public class ChangeCalculator
+{
+    public static int[] GiveMeMoney(int amount, int[] values)
+    {
+        var result = new int[values.Length];
+        for (var index = values.Length - 1; index >= 0; index--)
+        {
+            result[index] = amount / values[index];
+            amount %= values[index];
+        }
+        return result;
+    }
+}
