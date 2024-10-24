9a821fabd7003e775c292b5684200be0ff84803a
Will Smith <will@fundapps.co>
2024-10-24 11:32:56 +0100

    Bigger test case

--- Kata.Tests/UnitTest1.cs
+++ Kata.Tests/UnitTest1.cs
@@ -20,8 +20,11 @@ public class Tests
     [Test]
     public void Test3()
     {
-        var amount = ChangeCalculator.GiveMeMoney(314159265, [1, 2, 5, 10, 20, 50]);
+        var amount = ChangeCalculator.GiveMeMoney(314159265, [1, 2, 5, 10, 20, 50, 100, 200]);
         
-        var expected = new[] { 0, 0, 1, 0, 1, 0, 0 };
+        var expected = new[] { 0, 0, 1, 1, 0, 1, 0, 1570796 };
+        
+        Assert.That(amount, Is.EqualTo(expected));
+
     }
 }
