a3c061cd3380277e76e972930e988d58483649f6
Will Smith <will@fundapps.co>
2024-10-24 11:47:17 +0100

    Allow coins in any order

--- Kata.Tests/UnitTest1.cs
+++ Kata.Tests/UnitTest1.cs
@@ -27,4 +27,12 @@ public class Tests
         Assert.That(amount, Is.EqualTo(expected));
 
     }
+    
+    [Test]
+    public void Test4()
+    {
+        var amount = ChangeCalculator.GiveMeMoney(45, [5, 1, 2, 10, 20, 50]);
+        
+        Assert.That(amount, Is.EqualTo(new [] { 1, 0, 0, 0, 2, 0 }));
+    }
 }
--- Kata/ChangeCalculator.cs
+++ Kata/ChangeCalculator.cs
@@ -4,8 +4,11 @@ public class ChangeCalculator
 {
     public static int[] GiveMeMoney(int amount, int[] values)
     {
+        var indexOrder = values.Select((x, i) => (x, i)).OrderByDescending(x => x.x).Select(x => x.i);
+        
         var result = new int[values.Length];
-        for (var index = values.Length - 1; index >= 0; index--)
+        // for (var index = values.Length - 1; index >= 0; index--)
+        foreach(var index in indexOrder)
         {
             result[index] = amount / values[index];
             amount %= values[index];
